f28f2024d985e5e8c66d87e31d54918693ebf28b
Meet Khetani <158665731+EIFPE@users.noreply.github.com>
2024-04-21 20:54:52 +0530

    vids

--- src/app/courses/dataana/page.jsx
+++ src/app/courses/dataana/page.jsx
@@ -4,13 +4,20 @@ const VideoPage = () => {
   return (
     <div>
       <h1>Videos</h1>
-      <video controls width="560" height="315">
+      {/* <video controls width="560" height="315">
         <source
           src="https://raw.githubusercontent.com/Meet5703/TESTDATA/main/public/videos/vid1.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
+      <iframe
+        src="https://drive.google.com/file/d/10vApuo7xV2M9c5m47mJ3osL5zAMCegqh/preview"
+        width="560"
+        height="315"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
       <iframe
         src="https://drive.google.com/file/d/10fk8OkvTFaFZXoHBSvoCn_fPC-sk2PBz/preview"
         width="560"
@@ -18,22 +25,15 @@ const VideoPage = () => {
         frameborder="0"
         allowfullscreen
       ></iframe>
-      <video controls width="560" height="315">
-        <source
-          src="https://drive.google.com/file/d/10fk8OkvTFaFZXoHBSvoCn_fPC-sk2PBz/preview"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
 
-      <video controls width="560" height="315">
+      {/* <video controls width="560" height="315">
         <source src="/videos/vid3.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <video controls width="560" height="315">
         <source src="/videos/234.mp4" type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
     </div>
   );
 };

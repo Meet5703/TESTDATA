5d56e72e16258c17bdf784f4edf7489cffac553d
Meet Khetani <158665731+EIFPE@users.noreply.github.com>
2024-04-21 18:48:14 +0530

    23

--- src/app/courses/dataana/page.jsx
+++ src/app/courses/dataana/page.jsx
@@ -4,20 +4,14 @@ const VideoPage = () => {
   return (
     <div>
       <h1>Videos</h1>
-      <iframe
-        src="/public/videos/vid1.mp4"
-        title="Video Player"
-        width="560"
-        height="315"
-        allowFullScreen
-      ></iframe>
-      <iframe
-        src="/public/videos/vid3.mp4"
-        title="Video Player"
-        width="560"
-        height="315"
-        allowFullScreen
-      ></iframe>
+      <video controls width="560" height="315">
+        <source src="/videos/vid1.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <video controls width="560" height="315">
+        <source src="/videos/vid3.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };

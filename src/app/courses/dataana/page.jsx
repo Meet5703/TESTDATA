7d85545735c0015f6da2ed3da63923dcb03085b5
Meet Khetani <158665731+EIFPE@users.noreply.github.com>
2024-04-21 18:24:20 +0530

    123

--- src/app/courses/dataana/page.jsx
+++ src/app/courses/dataana/page.jsx
@@ -5,7 +5,7 @@ const VideoPage = () => {
     <div>
       <h1>Videos</h1>
 
-      <video controls controlsList="nodownload">
+      <video controls controlsList="nodownload" width="640" height="360">
         <source src="/videos/vid3.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>

8e2d94ad4f21ec5d0760c25288e4a305a42c0683
Meet Khetani <158665731+EIFPE@users.noreply.github.com>
2024-04-21 18:52:22 +0530

    234

--- src/app/courses/dataana/page.jsx
+++ src/app/courses/dataana/page.jsx
@@ -12,6 +12,10 @@ const VideoPage = () => {
         <source src="/videos/vid3.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <video controls width="560" height="315">
+        <source src="/videos/234.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
